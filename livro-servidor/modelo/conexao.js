363fa2cd2f648e2aa2d4caae8da2feb0211d802b
Christian Marques <145080782+christian-marques@users.noreply.github.com>
2026-02-02 23:47:19 -0300

    Configuração do servidor express

--- livro-servidor/modelo/conexao.js
+++ livro-servidor/modelo/conexao.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const banco = mongoose;
 
-const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-};
-
-banco.connect("mongodb://localhost:27017/livraria", options);
+banco.connect("mongodb://localhost:27017/livraria");
 
 module.exports = banco;
